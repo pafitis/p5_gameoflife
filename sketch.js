273ce1c7131a5ef9c086a08c544e2ed6870677f3
spafitis <savvazp@gmail.com>
2020-03-01 18:33:06 +0000

    fixed script

--- sketch.js
+++ sketch.js
@@ -8,17 +8,17 @@ function make2DArray(cols, rows){
   return arr;
 }
 
-function calcNext(grid, next){
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      let state = grid[i][j]
+function calcNext(grid, next) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
 
-      let sum = 0
+      let sum = 0;
       let neighbors = getNeighbours(grid, i, j);
 
-      if (state == 0 && neighbors == 3){
+      if (state == 0 && neighbors == 3) {
         next[i][j] = 1;
-      } else if (state == 1 & neighbors < 2 || neighbors > 3){
+      } else if (state == 1 & neighbors < 2 || neighbors > 3) {
         next[i][j] = 0;
       } else {
         next[i][j] = state;
@@ -28,10 +28,10 @@ function calcNext(grid, next){
   return next
 }
 
-function getNeighbours(grid, x, y){
+function getNeighbours(grid, x, y) {
   let sum = 0;
-  for (let i = -1; i < 2; i++){
-    for (let j = -1; j < 2; j++){
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
       let col = (x + i + cols) % cols;
       let row = (y + j + rows) % rows;
       sum += grid[col][row];
@@ -52,8 +52,8 @@ function setup() {
   rows = height / resolution;
 
   grid = make2DArray(cols, rows);
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       grid[i][j] = floor(random(2));
     }
   }
@@ -62,8 +62,8 @@ function setup() {
 function draw() {
   background(0);
 
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
       let x = i* resolution;
       let y = j* resolution;
       if (grid[i][j] == 1) {
@@ -78,6 +78,6 @@ function draw() {
   next = calcNext(grid, next);
 
   grid = next;
-  saveFrames('output/frame_####.pmg', 'png', framerate = 30)
+  // saveFrames('output/frame_####.pmg', 'png', framerate = 30)
 }
 
